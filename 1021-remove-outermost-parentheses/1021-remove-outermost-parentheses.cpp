533a06a54f3ddee41d6ae7a1c4ee4f256ac9984a
Harshit <harshit11082007@gmail.com>
2026-08-15 20:58:03 +0530

    Time: 0 ms (100%), Space: 8.9 MB (55.29%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int balance=0;
+        string ans="";
+        for(char c : s){
+            if(c=='('){
+                if(balance>0)
+                    ans+=c;
+                balance++;
+            }
+            else{balance--;
+                if(balance>0)
+                    ans+=c;
+                
+            }
+        }
+        return ans;
+    }
+};
