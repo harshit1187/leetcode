e70d47741e7c9297b120a5c851ffe402fd311c37
Harshit <harshit11082007@gmail.com>
2026-08-15 20:58:37 +0530

    Time: 0 ms (100%), Space: 8.8 MB (81.59%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int balance=0;
-        string ans="";
-        for(char c : s){
-            if(c=='('){
-                if(balance>0)
-                    ans+=c;
+        int balance = 0;
+        string ans = "";
+        for (char c : s) {
+            if (c == '(') {
+                if (balance > 0)
+                    ans += c;
                 balance++;
-            }
-            else{balance--;
-                if(balance>0)
-                    ans+=c;
-                
+            } else {
+                balance--;
+                if (balance > 0)
+                    ans += c;
             }
         }
         return ans;
